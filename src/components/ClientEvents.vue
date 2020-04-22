a7b8a4c9cdefafaf54597b762289e06441bad045
Sanjith <sanjith@pacbell.net>
2020-04-22 13:51:33 -0700

    Added ability to create custom events and add directly to upcoming events

--- src/components/ClientEvents.vue
+++ src/components/ClientEvents.vue
@@ -12,6 +12,55 @@
     <div class="field field--superwide">
       <EventsTable :events="pastEvents" :fetch-events="fetchEvents" />
     </div>
+    <div class="field">
+      <button
+        class="primary"
+        v-if="!createCustomEvent"
+        @click="createCustomEvent = true"
+      >
+        Create Custom Event
+      </button>
+    </div>
+    <form v-if="createCustomEvent" @submit.prevent="createEvent">
+      <h3>Event Name</h3>
+      <input type="text" v-model="newEvent.name" required />
+      <h3>Event Date</h3>
+      <input type="date" v-model="newEvent.date" required />
+      <h3>Event Time</h3>
+      <input type="time" v-model="newEvent.time" required />
+      <br />
+      <div class="field field--half" style="padding-top: 20px;">
+        <toggle-button
+          :value="newEvent.recurringEvent"
+          :color="{
+            checked: '#36d5d8',
+            unchecked: '#e52f2e',
+          }"
+          :labels="{
+            checked: 'Recurring Event',
+            unchecked: 'Single Event',
+          }"
+          :width="150"
+          :height="35"
+          :font-size="12"
+          @change="changeUserCreationPermission"
+          sync
+        />
+      </div>
+      <br />
+      <div v-if="newEvent.recurringEvent" style="padding-top: 50px;">
+        <h3>Days Between Events</h3>
+        <input type="number" min="1" v-model.number="newEvent.daysBetween" required />
+      </div>
+      <div class="field" style="padding-top: 10px;">
+        <button class="primary">
+          Save
+        </button>
+      </div>
+      <div class="field" style="padding-top: 10px;">
+        <button @click="cancel">Cancel</button>
+      </div>
+    </form>
   </section>
 </template>
 
@@ -21,13 +70,21 @@ import AV from "leancloud-storage";
 export default {
   name: "ClientEvents",
   components: {
-    EventsTable
+    EventsTable,
   },
   data() {
     return {
       upcomingEvents: [],
       suggestedEvents: [],
-      pastEvents: []
+      pastEvents: [],
+      createCustomEvent: false,
+      newEvent: {
+        name: "",
+        date: "",
+        time: "",
+        recurringEvent: false,
+        daysBetween: 0,
+      },
     };
   },
   created() {
@@ -47,8 +104,8 @@ export default {
         .include("client")
         .limit(1000)
         .find()
-        .then(upcomingEvents => {
-          vm.upcomingEvents = upcomingEvents.map(event => ({
+        .then((upcomingEvents) => {
+          vm.upcomingEvents = upcomingEvents.map((event) => ({
             event,
             editing: false,
             pendingChanges: {
@@ -59,11 +116,11 @@ export default {
                 .getDate()}`.slice(-2)}`,
               time: `${`0${event.get("time").getHours()}`.slice(
                 -2
-              )}:${`0${event.get("time").getMinutes()}`.slice(-2)}`
-            }
+              )}:${`0${event.get("time").getMinutes()}`.slice(-2)}`,
+            },
           }));
         })
-        .catch(error => {
+        .catch((error) => {
           alert(error);
         });
       const lastEventQuery = new AV.Query("Event");
@@ -77,8 +134,8 @@ export default {
         .include("client")
         .limit(1000)
         .find()
-        .then(lastEvents => {
-          vm.suggestedEvents = lastEvents.map(lastEvent => {
+        .then((lastEvents) => {
+          vm.suggestedEvents = lastEvents.map((lastEvent) => {
             const rawTime = new Date(
               new Date(lastEvent.get("time")).setDate(
                 lastEvent.get("time").getDate() + lastEvent.get("recursIn")
@@ -95,13 +152,13 @@ export default {
                   1}`.slice(-2)}-${`0${rawTime.getDate()}`.slice(-2)}`,
                 time: `${`0${rawTime.getHours()}`.slice(
                   -2
-                )}:${`0${rawTime.getMinutes()}`.slice(-2)}`
+                )}:${`0${rawTime.getMinutes()}`.slice(-2)}`,
               },
-              lastEvent
+              lastEvent,
             };
           });
         })
-        .catch(error => {
+        .catch((error) => {
           alert(error);
         });
       const pastEventQuery = new AV.Query("Event");
@@ -114,8 +171,8 @@ export default {
         .include("client")
         .limit(1000)
         .find()
-        .then(pastEvents => {
-          vm.pastEvents = pastEvents.map(event => ({
+        .then((pastEvents) => {
+          vm.pastEvents = pastEvents.map((event) => ({
             event,
             editing: false,
             pendingChanges: {
@@ -126,11 +183,11 @@ export default {
                 .getDate()}`.slice(-2)}`,
               time: `${`0${event.get("time").getHours()}`.slice(
                 -2
-              )}:${`0${event.get("time").getMinutes()}`.slice(-2)}`
-            }
+              )}:${`0${event.get("time").getMinutes()}`.slice(-2)}`,
+            },
           }));
         })
-        .catch(error => {
+        .catch((error) => {
           alert(error);
         });
     },
@@ -150,11 +207,64 @@ export default {
       event.lastEvent.unset("recursIn");
       AV.Object.saveAll([event.event, event.lastEvent])
         .then(vm.fetchEvents)
-        .catch(error => {
+        .catch((error) => {
           alert(error);
         });
-    }
-  }
+    },
+    createEvent() {
+      const vm = this;
+      console.log(vm.newEvent.date);
+      var ne = new AV.Object("Event");
+      ne.set("done", false);
+      ne.set("name", vm.newEvent.name);
+      ne.set(
+        "time",
+        new Date(
+          vm.newEvent.date.slice(0, 4),
+          vm.newEvent.date.slice(5, 7) - 1,
+          vm.newEvent.date.slice(8, 10),
+          vm.newEvent.time.slice(0, 2),
+          vm.newEvent.time.slice(3, 5),
+          0
+        )
+      );
+      var clientPointer = AV.Object.createWithoutData(
+        "Client",
+        vm.$route.params.id
+      );
+      ne.set("client", clientPointer);
+      if (vm.newEvent.recurringEvent == true) {
+        ne.set("recursIn", vm.newEvent.daysBetween);
+        console.log(vm.newEvent.daysBetween);
+      }
+      ne.save().then(
+        function(event) {
+          alert("New Event has been saved: " + event.id);
+          vm.resetNewEventValues();
+          vm.createCustomEvent = false;
+        },
+        function(error) {
+          alert(error);
+        }
+      );
+    },
+    changeUserCreationPermission(e) {
+      const vm = this;
+      vm.newEvent.recurringEvent = e.value;
+    },
+    cancel() {
+      const vm = this;
+      vm.resetNewEventValues();
+    },
+    resetNewEventValues() {
+      const vm = this;
+      vm.createCustomEvent = false;
+      vm.newEvent.name = "";
+      vm.newEvent.date = "";
+      vm.newEvent.time = "";
+      vm.newEvent.recurringEvent = false;
+    },
+  },
 };
 </script>
 
