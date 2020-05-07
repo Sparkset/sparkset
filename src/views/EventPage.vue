7fe18ecd29ea414dc856aa0b7b091fee76bfb7bc
Sanjith <sanjith@pacbell.net>
2020-05-06 19:17:36 -0700

    lol created event page

---
+++ src/views/EventPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="column">
+      <div class="card">
+        <section>
+          <h1>{{ event.get("name") }}</h1>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AV from "leancloud-storage";
+export default {
+  name: "EventPage",
+  data() {
+    return {
+      event: null
+    };
+  },
+  created() {
+    const vm = this;
+    const eventQuery = new AV.Query("Event");
+    const eventId = vm.$route.params.id;
+    eventQuery
+      .get(eventId)
+      .then(event => {
+        vm.event = event;
+      })
+      .catch(error => {
+        alert(error);
+      });
+  }
+};
+</script>
+
+<style scoped></style>
