02f4b18448b254addf76a27c9aba0c6647822052
Sanjith <sanjith@pacbell.net>
2020-05-08 13:00:30 -0700

    added ability to edit time and completion status

--- src/views/EventPage.vue
+++ src/views/EventPage.vue
@@ -2,16 +2,20 @@
   <div>
     <div class="column">
       <div class="card ">
-        <section class="fields title">
+        <section class="field fields title">
           <h1>{{ event.get("name") }}</h1>
-          <button class="primary" @click="toggle(event)">
+          <button
+            class="primary"
+            @click="toggle(event)"
+            style="margin-left: 10px;"
+          >
             {{ event.get("done") == true ? "Undone" : "Done" }}
           </button>
         </section>
-        <section class="fields">
+        <section class="field fields">
           <h2>Details</h2>
           <ul>
-            <li>
+            <li v-if="!editingTime">
               Time:
               {{
                 event.get("time").toLocaleString("en-US", {
@@ -22,9 +26,37 @@
                   minute: "numeric"
                 })
               }}
+              <button
+                class="primary"
+                v-if="!editingTime"
+                @click="editingTime = true"
+                style="margin-left: 10px;"
+              >
+                Edit Time
+              </button>
             </li>
+
+            <form v-if="editingTime" @submit.prevent="update(event)">
+              <h3>New Time</h3>
+              <div class="field">
+                <input
+                  type="date"
+                  max="2099-12-31"
+                  v-model="pendingChanges.date"
+                  required
+                />
+              </div>
+              <div class="field">
+                <input type="time" v-model="pendingChanges.time" required />
+              </div>
+              <div class="field">
+                <button type="submit" class="primary">
+                  Save
+                </button>
+              </div>
+            </form>
             <li v-if="event.get('client')">Client: {{ clientName }}</li>
-            <li>Company: {{ companyName }}</li>
+            <li>Client Company: {{ companyName }}</li>
           </ul>
         </section>
       </div>
@@ -40,13 +72,17 @@ export default {
     return {
       event: null,
       clientName: "",
-      companyName: ""
+      companyName: "",
+      editingTime: false,
+      pendingChanges: {
+        date: "",
+        time: ""
+      }
     };
   },
   methods: {
     getClientName(id) {
       const vm = this;
-      console.log(id);
       const clientQuery = new AV.Query("Client");
       clientQuery
         .get(id)
@@ -79,6 +115,46 @@ export default {
         .catch(error => {
           alert(error);
         });
+    },
+    update(event) {
+      const vm = this;
+      event.set(
+        "time",
+        new Date(
+          vm.pendingChanges.date.slice(0, 4),
+          vm.pendingChanges.date.slice(5, 7) - 1,
+          vm.pendingChanges.date.slice(8, 10),
+          vm.pendingChanges.time.slice(0, 2),
+          vm.pendingChanges.time.slice(3, 5),
+          0
+        )
+      );
+      event
+        .save()
+        .then(() => {
+          vm.editingTime = false;
+          vm.created;
+        })
+        .catch(error => {
+          alert(error);
+        });
+      // if (event.lastEvent) {
+      //   event.lastEvent.unset("recursIn");
+      //   AV.Object.saveAll([event.event, event.lastEvent])
+      //     .then(vm.fetchEvents)
+      //     .catch((error) => {
+      //       alert(error);
+      //     });
+      // } else {
+      //   event.event
+      //     .save()
+      //     .then(() => {
+      //       event.editing = false;
+      //     })
+      //     .catch((error) => {
+      //       alert(error);
+      //     });
+      // }
     }
   },
   created() {
